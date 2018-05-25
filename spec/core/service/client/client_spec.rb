d0aa8377f9c49b291c0a6d578d0c6ccfe1a6ac02
David Shaffer <dave.shaffer@gmail.com>
2018-05-25 10:27:15 -0400

    Resplat

--- spec/core/service/client/client_spec.rb
+++ spec/core/service/client/client_spec.rb
@@ -1,23 +1,31 @@
 RSpec.describe Hyper::Core::Service::Client do
   subject { Class.new { include Hyper::Core::Service::Client }.new }
 
-  let(:stub_dispatcher) { double('Dispatcher', call: {}) }
+  let(:endpoint) { 'foos' }
+  let(:args) { ['verify'] }
+  let(:kwargs) { {} }
+  let(:dispatcher) { Hyper::Core::Service::Dispatcher.new(request_method, endpoint, *args, kwargs) }
 
   before do
-    allow(Hyper::Core::Service::Dispatcher).to receive(:new).and_return(stub_dispatcher)
+    allow(dispatcher).to receive(:call)
+    allow(Hyper::Core::Service::Dispatcher).to receive(:new).and_return(dispatcher)
   end
 
   describe '#get' do
+    let(:request_method) { :get }
+
     it 'delegates to the dispatcher' do
-      subject.get('foos')
-      expect(stub_dispatcher).to have_received(:call)
+      subject.get(endpoint)
+      expect(dispatcher).to have_received(:call)
     end
   end
 
   describe '#post' do
+    let(:request_method) { :post }
+
     it 'delegates to the dispatcher' do
-      subject.post('foos')
-      expect(stub_dispatcher).to have_received(:call)
+      subject.post(endpoint)
+      expect(dispatcher).to have_received(:call)
     end
   end
 end
